e69c19988547c4c289602725165e1e2a03d24659
John Freeman <jcfree@mu2edaq13.fnal.gov>
2021-03-01 13:04:56 -0600

    JCF: have the_final_markdown.sh get its list of packages from the release manifest, and automatically edit mkdocs.yml so the desired markdown files get processed by MkDocs

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -2,11 +2,26 @@
 
 here=$(cd $(dirname $(readlink -f ${BASH_SOURCE})) && pwd)
 
-package_list="daq-buildtools ipm"
-markdown_dir="$here/../docs/packages"
+curl -O https://raw.githubusercontent.com/DUNE-DAQ/daq-release/develop/configs/dunedaq-v2.2.0/release_manifest
+
+package_list=$( sed -r -n '/^dune_packages=\(/,/\)/{s/^\s*"(\S+)\s+\S+\s+\S+\s*$/\1/p}' release_manifest | tr "\n" " " | tr "_" "-")
+package_list="$package_list daq-buildtools"
+
+packages_dir="$here/../docs/packages"
+mkdir -p $packages_dir
+
+tmpdir=$(mktemp -d)
+
+if [[ -d $tmpdir ]]; then
+    cd $tmpdir
+else
+    echo "Unable to create temporary directory $tmpdir; exiting..." >&2
+    exit 3
+fi
 
 for package in $package_list ; do
     
+    cd $tmpdir
     cmd="git clone https://github.com/DUNE-DAQ/$package"
     $cmd
     
@@ -15,34 +30,43 @@ for package in $package_list ; do
 	exit 1
     fi
 
-    cd $package
-    git checkout johnfreeman/dont-readme 2>/dev/null
-    
+    cd $tmpdir/$package
+    git checkout johnfreeman/dont-readme 2>/dev/null  # OK if this fails, just trying to see if the branch is available
+    echo $tmpdir/$package
 
-    if [[ -d docs/ ]]; then
-	echo "Found a docs/ subdirectory, ignoring any README.md in the base of the repo"
-	cd docs/
-	cp -rp . $markdown_dir/$package 
+    if [[ -d $tmpdir/$package/docs/ ]]; then
+	echo "Found a docs/ subdirectory in repo $package, ignoring any README.md in the base of the repo"
 
-	if [[ "$?" == 0 ]]; then
-	    cd ..
-	else
-	    echo "There was a problem copying the contents of $PWD into $markdown_dir/$package ; exiting..." >&2
+	mkdir -p $packages_dir/$package
+	cp -rp $tmpdir/$package/docs/* $packages_dir/$package 
+
+	if [[ "$?" != 0 ]]; then
+	    echo "There was a problem copying the contents of $PWD into $packages_dir/$package ; exiting..." >&2
 	    exit 2
 	fi
 
-	for mdfile in $( find $markdown_dir/$package -name "*.md" ); do
-	    sed -r -i '/^\s*nav\s*:.*/a \    - PageName: $mdfile' $here/../mkdocs.yml
+	for mdfile in $( find $packages_dir/$package -name "*.md" ); do
+	    echo "Handling $mdfile"
+	    pagename=$package/$( echo $mdfile | sed -r 's!^.*/(.*).md$!\1!' )
+	    mdfile_relative=$( echo $mdfile | sed -r 's!^.*/docs/(.*)!\1!' )
+	    sed -r -i '/^\s*nav\s*:.*/a \    - '$pagename': '$mdfile_relative $here/../mkdocs.yml
 	done
 
-	find $markdown_dir/$package -type f -not -name "*.md" | xargs -i rm -f {}
+	find $packages_dir/$package -type f -not -name "*.md" | xargs -i rm -f {}  # What about *.png, etc.?
     else # No docs/ directory found
-	if [[ -e README.md ]]; then
-	    mkdir -p $markdown_dir/$package 
-	    cp -p README.md $markdown_dir/$package
+	if [[ -e $tmpdir/$package/README.md ]]; then
+	    mkdir -p $packages_dir/$package 
+	    cp -p $tmpdir/$package/README.md $packages_dir/$package
+	    mdfile=$packages_dir/$package/README.md 
+	    mdfile_relative=$( echo $mdfile | sed -r 's!^.*/docs/(.*)!\1!' )
+	    sed -r -i '/^\s*nav\s*:.*/a \    - '$package': '$mdfile_relative $here/../mkdocs.yml
 	else
 	    echo "No docs/ subdirectory or README.md found for $package; no documentation will be generated" >&2
 	fi
     fi
 
 done
+
+if [[ -d $tmpdir && "$tmpdir" =~ ^/tmp/.*$ ]]; then
+    rm -rf $tmpdir
+fi
