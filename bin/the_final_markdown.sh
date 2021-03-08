be7a3b444cba5daddc2f47b87f81854d2b2d7d6a
John Freeman <jcfree@mu2edaq13.fnal.gov>
2021-03-07 22:54:38 -0600

    JCF: update the_final_markdown.sh so it adds titles to the html pages for more accurate markdown translation, and put packages in alphabetic (as opposed to reverse alphabetic) order

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 here=$(cd $(dirname $(readlink -f ${BASH_SOURCE})) && pwd)
-tag=dunedaq-v2.2.0
+#tag=dunedaq-v2.3.0
 
-curl -O https://raw.githubusercontent.com/DUNE-DAQ/daq-release/develop/configs/$tag/release_manifest
-
-#package_list=$( sed -r -n '/^dune_packages=\(/,/\)/{s/^\s*"(\S+)\s+\S+\s+\S+\s*$/\1/p}' release_manifest | tr "\n" " " | tr "_" "-")
-#package_list="$package_list daq-buildtools"
-#rm -f release_manifest
-package_list="appfwk cmdlib daq-buildtools daq-cmake daqdemos dataformats dfmessages dfmodules ers ipm listrev logging minidaqapp nwqueueadapters opmonlib rcif readout restcmd serialization trigemu"
+# Reverse alphabetical order so the packages in the drop-down menu will appear in regular alphabetical order
+package_list="trigemu serialization restcmd readout rcif opmonlib nwqueueadapters minidaqapp logging listrev ipm ers dfmodules dfmessages dataformats daqdemos daq-cmake daq-buildtools cmdlib appfwk"
 
 
 packages_dir="$here/../docs/packages"
@@ -31,10 +27,21 @@ fi
 # MkDocs won't interpret it as a bullet unless there's an empty line
 # above it.
 
-function rinse() {
+function massage() {
 
-    sed -r -i 's/^(\*.*)$/\n\1/;s/^ {3,4}(\*.*)/    \1/;;s/^ {5,}(\*.*)/        \1/' $1
-    sed -r -i 's/^([0-9]+\..*)/\n\1/' $1
+    markdown_file=$1
+
+    if ! [[ "$markdown_file" =~ .*README.md$ ]]; then
+	header=$( echo $markdown_file | sed -r 's!.*/(.*)!\1!;s/\-/ /g;s/\.md$//' )
+    else
+	package=$( echo $markdown_file | sed -r 's!.*/([^/]+)/README.md!\1!' )
+	header="$package README"
+    fi
+
+    echo "header is \"$header\""
+    sed -r -i "1s/^/# $header\n/" $markdown_file
+    sed -r -i 's/^(\*.*)$/\n\1/;s/^ {3,4}(\*.*)/    \1/;;s/^ {5,}(\*.*)/        \1/' $markdown_file
+    sed -r -i 's/^([0-9]+\..*)/\n\1/' $markdown_file
 }
 
 for package in $package_list ; do
@@ -49,7 +56,7 @@ for package in $package_list ; do
     fi
 
     cd $tmpdir/$package
-    git checkout johnfreeman/dont-readme 2>/dev/null  # OK if this fails, just trying to see if the branch is available
+    git checkout johnfreeman/dont-readme 2>/dev/null  # OK if this fails, just trying to see if the branch is available for developer purposes
     echo $tmpdir/$package
 
     if [[ -d $tmpdir/$package/docs/ || -e $tmpdir/$package/README.md ]]; then
@@ -72,7 +79,7 @@ for package in $package_list ; do
 
 	for mdfile in $( find $packages_dir/$package -name "*.md" ); do
 	    echo "Handling $mdfile"
-	    rinse $mdfile
+	    massage $mdfile
 	    pagename=$( echo $mdfile | sed -r 's!^.*/(.*).md$!\1!' )
 	    mdfile_relative=$( echo $mdfile | sed -r 's!^.*/docs/(.*)!\1!' )
 	    sed -r -i '/^\s*-\s*'$package'\s*:.*/a \          - '$pagename': '$mdfile_relative $here/../mkdocs.yml
@@ -82,7 +89,7 @@ for package in $package_list ; do
 	if [[ -e $tmpdir/$package/README.md ]]; then
 	    cp -p $tmpdir/$package/README.md $packages_dir/$package
 	    mdfile=$packages_dir/$package/README.md 
-	    rinse $mdfile
+	    massage $mdfile
 	    mdfile_relative=$( echo $mdfile | sed -r 's!^.*/docs/(.*)!\1!' )
 	    sed -r -i 's!^(\s*)-(\s*)'$package'(\s*):!\1-\2'$package'\3: '$mdfile_relative'!' $here/../mkdocs.yml
 	fi
