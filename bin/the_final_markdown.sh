416c75daa8b3bc054b9a29c40480870943e71bdf
John Freeman <jcfree@mu2edaq13.fnal.gov>
2021-03-10 14:33:15 -0600

    JCF: updated the_final_markdown.sh adds in the styleguide and daq-release, and handles numbered lists/sublists

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 here=$(cd $(dirname $(readlink -f ${BASH_SOURCE})) && pwd)
-#tag=dunedaq-v2.3.0
 
 # Reverse alphabetical order so the packages in the drop-down menu will appear in regular alphabetical order
-package_list="trigemu serialization restcmd readout rcif opmonlib nwqueueadapters minidaqapp logging listrev ipm ers dfmodules dfmessages dataformats daqdemos daq-cmake daq-buildtools cmdlib appfwk"
+package_list="trigemu styleguide serialization restcmd readout rcif opmonlib nwqueueadapters minidaqapp logging listrev ipm ers dfmodules dfmessages dataformats daq-release daqdemos daq-cmake daq-buildtools cmdlib appfwk"
 
 
 packages_dir="$here/../docs/packages"
@@ -39,8 +38,10 @@ function massage() {
     fi
 
     sed -r -i "1s/^/# $header\n/" $markdown_file
-    sed -r -i 's/^(\*.*)$/\n\1/;s/^ {3,4}(\*.*)/    \1/;;s/^ {5,}(\*.*)/        \1/' $markdown_file
+    sed -r -i 's/^(\*.*)$/\n\1/;s/^ {3,4}(\*.*)/    \1/;s/^ {5,}(\*.*)/        \1/' $markdown_file
     sed -r -i 's/^([0-9]+\..*)/\n\1/' $markdown_file
+    sed -r -i 's/^([0-9]+\..*)/\n\1/;s/^ {3,4}([0-9]+\.*)/    \1/;' $markdown_file
+
 }
 
 for package in $package_list ; do
