ea53942aff29429d02a7e9d1a6a444c67f7ee278
Pengfei Ding <dingpf@docker-bd.fnal.gov>
2021-08-17 15:19:49 -0500

    using dunedaq-v2.8.0 tag for daq-cmake and daq-b-buildtools for the latest version of docs

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -119,7 +119,7 @@ for package in $package_list ; do
     # want the latest-greatest
 
     if [[ "$package" =~ "daq-buildtools" || "$package" =~ "daq-cmake" ]]; then
-	git checkout dunedaq-v2.6.0
+	git checkout dunedaq-v2.8.0
     else
 	git checkout develop
     fi
