247fd4e01e6d707b54aacc0aff1f816f32ef790d
John Freeman <jcfree@mu2edaq13.fnal.gov>
2022-10-11 09:23:12 -0500

    JCF: add updated documentation for daq-buildtools now that dunedaq-v3.2.0 is available

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -117,9 +117,9 @@ for package in $package_list ; do
     # want the latest-greatest
 
     if [[ "$package" =~ "daq-buildtools" ]]; then
-	git checkout dunedaq-v3.2.0_candidate_for_docs
+	git checkout dunedaq-v3.2.0_for_docs
     elif [[ "$package" =~ "daq-cmake" ]]; then
-	git checkout dunedaq-v3.2.0_candidate_for_docs
+	git checkout dunedaq-v3.2.0_for_docs
     else
 	git checkout develop
     fi
