3a9c831f6a35b974a677d3c7794a6f555866c95e
John Freeman <jcfree@daq.fnal.gov>
2025-07-16 10:53:07 -0500

    JCF: bump documentation to reflect new daq-cmake

--- bin/the_final_markdown.sh
+++ bin/the_final_markdown.sh
@@ -117,7 +117,7 @@ for package in $package_list ; do
     if [[ "$package" =~ "daq-buildtools" ]]; then
 	git checkout v8.9.4_for_docs
     elif [[ "$package" =~ "daq-cmake" ]]; then
-	git checkout v3.1.0
+	git checkout v3.1.1
     fi
     echo $tmpdir/$package
 
